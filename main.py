32e76c4b3125868376588bb8439e53317d330b0c
Chris <syhbstk@gmail.com>
2025-09-27 00:17:43 -0700

    add files and comments in the main.py

--- main.py
+++ main.py
@@ -13,10 +13,12 @@ class BMPViewer(QWidget):
         self.setWindowTitle("BMP Viewer")
         self.resize(700, 500)
 
+        # Store original pixel data and image size
         self.original_pixels = None
         self.width = 0
         self.height = 0
 
+        # RGB channels toggle and display settings
         self.r_enabled = True
         self.g_enabled = True
         self.b_enabled = True
@@ -27,12 +29,15 @@ class BMPViewer(QWidget):
 
         top_layout = QHBoxLayout()
 
+        # Button to open BMP file
         self.open_button = QPushButton("Open BMP File")
         self.open_button.setFixedSize(120, 50)
         self.open_button.clicked.connect(self.open_file)
         top_layout.addWidget(self.open_button)
 
         top_layout.addStretch()
+
+        # Checkboxes to enable/disable R, G, B channels
         self.r_button = QCheckBox("R")
         self.g_button = QCheckBox("G")
         self.b_button = QCheckBox("B")
@@ -48,16 +53,19 @@ class BMPViewer(QWidget):
 
         layout.addLayout(top_layout)
 
+        # Label to display the image
         self.image_label = QLabel("No Image Loaded")
         self.image_label.setStyleSheet("border: 1px solid black; background: white;")
         self.image_label.setAlignment(Qt.AlignCenter)  
         self.image_label.setFixedSize(700, 400)
         layout.addWidget(self.image_label)
 
+        # Text box to display BMP metadata
         self.metadata_box = QTextEdit("No Metadata Loaded")
         self.metadata_box.setReadOnly(True)
         layout.addWidget(self.metadata_box)
 
+        # Slider for brightness adjustment
         self.brightness_slider = QSlider(Qt.Horizontal)
         self.brightness_slider.setRange(0, 100)
         self.brightness_slider.setValue(100)
@@ -65,6 +73,7 @@ class BMPViewer(QWidget):
         layout.addWidget(QLabel("Brightness"))
         layout.addWidget(self.brightness_slider)
 
+        # Slider for scaling the image
         self.scale_slider = QSlider(Qt.Horizontal)
         self.scale_slider.setRange(1, 100)
         self.scale_slider.setValue(100)
@@ -74,6 +83,7 @@ class BMPViewer(QWidget):
 
         self.setLayout(layout)
 
+    # Open BMP file and load pixel data
     def open_file(self):
         filepath, _ = QFileDialog.getOpenFileName(self, "Open BMP File", "", "BMP Files (*.bmp)")
         if not filepath:
@@ -82,17 +92,20 @@ class BMPViewer(QWidget):
         parser = BMPParser(filepath)
         parser.load()
 
+        # Display metadata
         meta_text = ""
         for k, v in parser.metadata.items():
             meta_text += f"{k}: {v}\n"
         self.metadata_box.setText(meta_text)
 
+        # Store pixel data and image size
         self.original_pixels = parser.pixel_data
         self.width = parser.metadata["width"]
         self.height = abs(parser.metadata["height"])
 
         self.update_image()
 
+    # Update image display based on settings
     def update_image(self):
         if self.original_pixels is None:
             return
@@ -105,9 +118,9 @@ class BMPViewer(QWidget):
 
         image = QImage(new_w, new_h, QImage.Format_RGB32)
 
+        # Loop through each pixel and apply brightness and RGB toggle
         for y in range(new_h):
             for x in range(new_w):
-                
                 src_x = int(x / self.scale)
                 src_y = int(y / self.scale)
 
@@ -126,17 +139,21 @@ class BMPViewer(QWidget):
 
                 image.setPixel(x, y, qRgb(R, G, B))
 
+        # Show updated image
         pixmap = QPixmap.fromImage(image)
         self.image_label.setPixmap(pixmap)
         
+    # Toggle R channel
     def toggle_r(self):
         self.r_enabled = self.r_button.isChecked()
         self.update_image()
 
+    # Toggle G channel
     def toggle_g(self):
         self.g_enabled = self.g_button.isChecked()
         self.update_image()
 
+    # Toggle B channel
     def toggle_b(self):
         self.b_enabled = self.b_button.isChecked()
         self.update_image()
